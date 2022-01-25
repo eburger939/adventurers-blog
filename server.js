cedbfbbd7deea7228a023898faeca1c40915db5f
Emily Burger <eburger939@gmail.com>
2022-01-24 19:48:04 -0600

    added connect session sequelize to server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 require('dotenv').config()
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
+const { strict } = require('assert');
 const routes = require('./controllers')
 const sequelize = require('./config/connection')
 const helpers = require('./utils/helpers')
@@ -15,6 +17,17 @@ const sess = {
   secret: process.env.DB_SECRET,
   resave: false,
   saveUninitialized: true,
+  cookie: {
+      maxAge: 3600,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize,
+  }),
 };
 
 app.use(session(sess));
